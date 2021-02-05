ceb0bc6b6a5cdccf6874a840a37dfae1e3915ddd
YiowHur_Ng <yiowhur_ng@asus.com>
2021-02-05 17:00:26 +0800

    [LTE] Implement LTE udev rules, remove init script
    Change-Id: I80314d2c29662f6220362b165b202317ac42875b

--- recipes-bsp/asus-overlay/files/common/sbin/lte.sh
+++ recipes-bsp/asus-overlay/files/common/sbin/lte.sh
@@ -2,6 +2,7 @@
 
 CONFIG_PATH=/etc/lte
 CONFIG_FILE=${CONFIG_PATH}/lte.conf
+TAG="LTE"
 
 check_config() {
     # Check config exists
@@ -17,6 +18,35 @@ check_config() {
     fi
 }
 
+start_quectel_tool() {
+	. $CONFIG_FILE
+	if [ "$AUTO_CONNECT" == "y" ]; then
+		if [ "$APN" == "" ]; then
+			if [ "$PIN" == "" ]; then
+				quectel-CM &
+			else
+				quectel-CM -p $PIN &
+			fi
+		else
+			if [ "$PIN" == "" ]; then
+				quectel-CM -s $APN &
+			else
+				quectel-CM -s $APN -p $PIN &
+			fi
+		fi
+		PID=`pidof quectel-CM`
+		log "=== Start at ${PID} ==="
+	else
+		log "AUTO_CONNECT not enabled"
+	fi
+}
+
+log() {
+	echo ${1}
+	logger -t ${TAG} ${1}
+}
+
+log "do ${1}"
 case $1 in
 set-apn) 
     check_config
@@ -31,18 +61,37 @@ set-auto)
         check_config
         sed -i "s/\(AUTO_CONNECT *= *\).*/\1$2/" $CONFIG_FILE
     else
-        echo "Parameter error"
+        log "Parameter error"
+    fi
+    ;;
+set-sim)
+    if [ "$2" == "0" -o "$2" == "1" ]; then
+        ex_gpio -s ${2}
+        echo -ne "AT+CFUN=1,1\r" | microcom /dev/ttyUSB2
+    else
+        log "Parameter error"
     fi
     ;;
 reboot-module)
-    echo -ne "AT+CFUN=1,1\r" | microcom -t 5000 /dev/ttyUSB2
+    echo -ne "AT+CFUN=1,1\r" | microcom /dev/ttyUSB2
     ;;
 reset)
     echo "APN=internet" > $CONFIG_FILE
     echo "PIN=0000" >> $CONFIG_FILE
     echo "AUTO_CONNECT=y" >> $CONFIG_FILE
     ;;
-*)
-    echo "Usage: $0 {set-apn [apn]| set-pin [pin]| set-auto [y/n]| reset}"
+start)
+    check_config
+    start_quectel_tool
     ;;
+stop)
+    log "=== Disconnect ==="
+    PID=`pidof quectel-CM`
+    if [ "${PID}" != "" ]; then
+        kill ${PID}
+    fi
+    ;;
+*)
+    echo "Usage: $0 {set-apn [apn]| set-pin [pin]| set-auto [y/n]| set-sim [0/1]| reset| reboot-module| start| stop}"
+    ;; 
 esac
--- recipes-core/initscripts/initscripts_%.bbappend
+++ recipes-core/initscripts/initscripts_%.bbappend
@@ -1,17 +1,14 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${BPN}:"
 
 SRC_URI += "file://rtcinit.sh \
-            file://lteinit.sh \
             file://audioinit.sh \
             file://adbd.sh \
 "
 
 do_install_append () {
   install -m 0755 ${WORKDIR}/rtcinit.sh ${D}${sysconfdir}/init.d
-  install -m 0755 ${WORKDIR}/lteinit.sh ${D}${sysconfdir}/init.d
   install -m 0755 ${WORKDIR}/adbd.sh ${D}${sysconfdir}/init.d
   update-rc.d -r ${D} rtcinit.sh start 50 2 3 4 5 .
-  update-rc.d -r ${D} lteinit.sh start 51 2 3 4 5 .
   update-rc.d -r ${D} adbd.sh start 40 S . stop 01 6 .
 }
 
