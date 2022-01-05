695fa3d221f9e94b73f69d50e6e2cea93a6c477d
vic_hsu <Vic_Hsu@asus.com>
2022-01-05 18:13:28 +0800

    Compress the Image for EdgeX and delete ImageforEdgeX.tar.
    Change-Id: I23b14e2559dd510b3e3acec9e7ae864dd5172eef
    
    Former-commit-id: a97ddb1834e0bfaf18906f175e0cbd611d5db5e9

--- recipes-bsp/asus-overlay/files/common/etc/EdgeX/EdgeX.sh
+++ recipes-bsp/asus-overlay/files/common/etc/EdgeX/EdgeX.sh
@@ -6,16 +6,25 @@ edgexfoundry/core-data-arm64 edgexfoundry/support-notifications-arm64
 edgexfoundry/sys-mgmt-agent-arm64 edgexfoundry/core-metadata-arm64 edgexfoundry/core-command-arm64 
 edgexfoundry/support-scheduler-arm64 consul)
 
+image_tar_gz=(edgexfoundry_ekuiper edgexfoundry_app-service-configurable-arm64 edgexfoundry_redis 
+edgexfoundry_edgex-ui-arm64 edgexfoundry_device-rest-arm64 edgexfoundry_device-virtual-arm64 
+edgexfoundry_core-data-arm64 edgexfoundry_support-notifications-arm64 
+edgexfoundry_sys-mgmt-agent-arm64 edgexfoundry_core-metadata-arm64 edgexfoundry_core-command-arm64 
+edgexfoundry_support-scheduler-arm64 edgexfoundry_consul)
+
+loop=0
+
 for i in "${images[@]}";
 do
     image=$(docker images | grep $i)
 
     if [ "$image" = "" ]; then
         echo "I can not find the image $i."
-        docker load -i /etc/EdgeX/ImageforEdgeX.tar
-        break
+        docker load < /etc/EdgeX/EdgeXImages/${image_tar_gz[$loop]}.tar.gz
+        loop=$(($loop +1))
     else
         echo "image = $image"
+        loop=$(($loop +1))
     fi
 done
 
